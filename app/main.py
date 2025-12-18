a9d409938ff5bc6d70144cf582a4243b3b1b876c
StychukMaria <stychuk.maria@gmail.com>
2025-12-19 00:39:23 +0200

    ChangeSolution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
-    def wrapper(*args, **kwargs):
-        if (tuple(args), tuple(sorted(kwargs.items()))) in cache_dict:
+
+    def wrapper(*args, **kwargs) -> Any:
+        cache_key = (tuple(args), tuple(sorted(kwargs.items())))
+        if cache_key in cache_dict:
             print("Getting from cache")
         else:
-            cache_dict[(tuple(args), tuple(sorted(kwargs.items())))] = func(*args, **kwargs)
+            cache_dict[cache_key] = func(*args, **kwargs)
             print("Calculating new result")
-        return cache_dict[(tuple(args), tuple(sorted(kwargs.items())))]
+        return cache_dict[cache_key]
     return wrapper
