e8aa1c4d322038dd120010cc6198f1ad44bd4396
StychukMaria <stychuk.maria@gmail.com>
2025-12-19 00:31:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,12 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+    def wrapper(*args, **kwargs):
+        if (tuple(args), tuple(sorted(kwargs.items()))) in cache_dict:
+            print("Getting from cache")
+        else:
+            cache_dict[(tuple(args), tuple(sorted(kwargs.items())))] = func(*args, **kwargs)
+            print("Calculating new result")
+        return cache_dict[(tuple(args), tuple(sorted(kwargs.items())))]
+    return wrapper
